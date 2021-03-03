4c7435a173f4b254156b01133242a261dfe94a33
Ivan Kashin <barbos1701@gmail.com>
2021-03-03 08:07:22 +0300

    Адаптирует шапку для популярных размеров

--- src/js/components/header/header.jsx
+++ src/js/components/header/header.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const Header = () => {
   return (
     <header className="page-header">
-      <h1 className="page-header__title">Decision maker</h1>
-      <p className="page-header__title-description">This app will allow you to check if your decision is profitable or not.</p>
+      <div className="page-header__wrapper">
+        <h1 className="page-header__title">Decision maker</h1>
+        <p className="page-header__title-description">Check if your decision is profitable or not.</p>
+      </div>
     </header>
   );
 };
